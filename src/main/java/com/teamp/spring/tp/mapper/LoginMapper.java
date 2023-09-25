417b1b2ee11b4b18537bf4b3f060d0cdd1fbcfe9
namjunu <hsson040@gmail.com>
2023-09-25 11:07:00 +0900

    commit
    login 제외후 commit merge하기 위함

--- src/main/java/com/teamp/spring/tp/mapper/LoginMapper.java
+++
@@ -1,8 +0,0 @@
-package com.teamp.spring.tp.mapper;
-
-import com.teamp.spring.tp.dto.UserInfo;
-
-public interface LoginMapper {
-	public void createID(UserInfo id);
-	public boolean LoginTest(String U_ID,String U_PW);
-}
--- src/main/java/com/teamp/spring/tp/service/LoginService.java
+++
@@ -1,7 +0,0 @@
-package com.teamp.spring.tp.service;
-
-import com.teamp.spring.tp.dto.UserInfo;
-
-public interface LoginService {
-	public void createID(UserInfo id);
-}
